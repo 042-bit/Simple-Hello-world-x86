ee98931d0fcd85cd486694fb550614e19559e704
042 <kokosk399@gmail.com>
2026-04-10 22:40:21 +0200

    Add hello_world.asm to print a message

---
+++ hello_world.asm
@@ -0,0 +1,19 @@
+section .data 
+
+msg db "hello_world",10
+msg_len equ $ - msg
+
+section .text
+global start_
+
+start:
+
+  mov rax, 1
+  mov rdi, 1 
+  mov rsi, msg
+  mov rdx, msg_len
+  syscall 
+  
+  mov rax, 60 
+  xor rdi, rdi
+  syscall 
